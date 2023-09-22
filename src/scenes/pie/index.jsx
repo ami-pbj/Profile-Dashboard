20cc03e82840ee915d8f492fbf207f4fc9903954
PURNENDU BIKASH JANA <purnendubikash.21@gmail.com>
2023-09-22 19:05:03 +0530

    Completed Pie Chart Section

--- src/scenes/pie/index.jsx
+++ src/scenes/pie/index.jsx
@@ -1,7 +1,17 @@
+import { Box } from "@mui/material";
 import React from "react";
+import Header from "../../components/Header";
+import PieChart from "../../components/PieChart";
 
 const Pie = () => {
-  return <div>Pie</div>;
+  return (
+    <Box m="20px">
+      <Header title="Bar Chart" subtitle="Simple Bar Chart" />
+      <Box height="75vh">
+        <PieChart />
+      </Box>
+    </Box>
+  );
 };
 
 export default Pie;
