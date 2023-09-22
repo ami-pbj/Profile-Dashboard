fbfe52c90b16f0a4748c899206b938015c9419cd
PURNENDU BIKASH JANA <purnendubikash.21@gmail.com>
2023-09-22 20:49:54 +0530

    Designed First Row of Dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -3,37 +3,129 @@ import Header from "../../components/Header";
 import { Box, Button, useTheme } from "@mui/material";
 import ProgressCircle from "../../components/ProgressCircle";
 import StatBox from "../../components/StatBox";
-
-import { EmailIcon } from "@mui/icons-material/Email";
-import { PointOfSaleIcon } from "@mui/icons-material/PointOfSale";
-import { PersonAddIcon } from "@mui/icons-material/PersonAdd";
-import { TrafficIcon } from "@mui/icons-material/Traffic";
 import { tokens } from "../../theme";
-import { DownloadOutlined } from "@mui/icons-material";
+import {
+  DownloadOutlined,
+  Email,
+  PersonAdd,
+  PointOfSale,
+  Traffic,
+} from "@mui/icons-material";
 
 const Dashboard = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
   return (
-    <Box m="20px" display="flex" justifyContent="space-between">
+    <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="Dashboard" subtitle="Welcome to Admin Dashboard !!" />
+
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlined sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box>
       </Box>
 
-      <Box>
-        <Button
-          sx={{
-            backgroundColor: colors.blueAccent[700],
-            color: colors.grey[100],
-            fontSize: "14px",
-            fontWeight: "bold",
-            padding: "10px 20px",
-          }}
+      {/* GRID & CHARTS */}
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="140px"
+        gap="20px"
+      >
+        {/* ROW 1 */}
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
         >
-          <DownloadOutlined sx={{ mr: "10px" }} />
-          Download Reports
-        </Button>
+          <StatBox
+            title="12,361"
+            subtitle="Emails Sent"
+            progress="0.75"
+            increase="+14%"
+            icon={
+              <Email
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="32,361"
+            subtitle="Sales Obtained"
+            progress="0.50"
+            increase="+21%"
+            icon={
+              <PointOfSale
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="32,441"
+            subtitle="New Clients"
+            progress="0.30"
+            increase="+5%"
+            icon={
+              <PersonAdd
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="1,325,134"
+            subtitle="Traffic Inbound"
+            progress="0.80"
+            increase="+43%"
+            icon={
+              <Traffic
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        {/* ROW 2 */}
       </Box>
     </Box>
   );
